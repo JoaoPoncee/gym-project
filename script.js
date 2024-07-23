a186885c3ad582beb7dbc1bcf979eb1e3992a3c4
CotejaDoido <joaovictor.poncelima1@gmail.com>
2024-07-23 16:32:04 -0300

    fix

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const nameExercise = document.querySelector(".input-form-exercise");
+const repeatExercise = document.querySelector(".input-form-repeat");
+const seriesExercise = document.querySelector(".input-form-series");
+const btnWeek = document.querySelectorAll(".btn-week");
+const btnAdd = document.querySelector("#btn-add");
+
 function pageNavigation(day) {
   const novaURL = `add-training.html?day=${day}`;
   window.location.href = novaURL;
@@ -10,9 +16,9 @@ function backPage() {
 function openModal() {
   const modal = document.querySelector(".modal");
   const arrowBack = document.querySelector(".img-arrow, .back-home");
+  const actualStyle = modal.style.display;
   arrowBack.style.display = "none";
   modal.style.display = "flex";
-  const actualStyle = modal.style.display;
   if (actualStyle == "block") {
     modal.style.display = "none";
   } else {
@@ -25,17 +31,10 @@ function closeModal() {
   const arrowBack = document.querySelector(".img-arrow, .back-home");
   arrowBack.style.display = "block";
   modal.style.display = "none";
-  nameExercise.value = " "
-  repeatExercise.value = " "
-  seriesExercise.value = " "
+  nameExercise.value = " ";
+  repeatExercise.value = " ";
+  seriesExercise.value = " ";
 }
-
-const nameExercise = document.querySelector(".input-form-exercise");
-const repeatExercise = document.querySelector(".input-form-repeat");
-const seriesExercise = document.querySelector(".input-form-series");
-
-const btnWeek = document.querySelectorAll(".btn-week");
-const btnAdd = document.querySelector("#btn-add");
 
 btnAdd.addEventListener("click", (e) => {
   e.preventDefault();
@@ -54,5 +53,4 @@ btnAdd.addEventListener("click", (e) => {
   // Salva a lista atualizada no localStorage
   localStorage.setItem(day, JSON.stringify(training));
   closeModal();
-
 });
